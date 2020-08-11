c21d6ebdefd07f022ed43c9d32ab8a2ac8a33962
Taylor Riley Du <taylorsriley@gmail.com>
2020-08-11 12:43:59 -0400

    Webpack Progress

--- server/server.js
+++ server/server.js
@@ -2,4 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
 app.listen(3000);
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,64 @@
+const webpack = require('webpack');
 const path = require('path');
 
-module.exports = {};
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+};
+
+/* EXAMPLE WEBPACK
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    publicPath: 'http://localhost:8080/build/',
+    proxy: {
+      '/api/leaders': 'http://localhost:3000'
+    }
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+};
+*/
