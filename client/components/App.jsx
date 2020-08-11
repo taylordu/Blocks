32b315711f87cb8d77687353504af9a598e73e47
Taylor Riley Du <taylorsriley@gmail.com>
2020-08-11 19:49:40 -0400

    Added React Component Library + Component Files

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {};
   }
   render() {
     return (
       <div className="today">
-        <h1>Today</h1>
+        <h2>Today</h2>
+        <List>
+          <ListItem>
+            <Checkbox />
+            <ListItemText primary="Learn React" />
+          </ListItem>
+          <ListItem>
+            <Checkbox />
+            <ListItemText primary="Make a list" />
+          </ListItem>
+          <ListItem>
+            <Checkbox />
+            <ListItemText primary="Drag and drop" />
+          </ListItem>
+        </List>
       </div>
     );
   }
