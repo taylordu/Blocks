787e5df5ffe453cb3699b1656c40d6015ef53712
Taylor Riley Du <taylorsriley@gmail.com>
2020-08-12 11:32:43 -0400

    Button + Add Schedule Functionality Working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,128 +1,44 @@
 import React, { Component } from 'react';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Checkbox from '@material-ui/core/Checkbox';
+import Schedule from './Schedule.jsx';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      schedules: [1, 2],
+    };
+    this.renderSchedule = this.renderSchedule.bind(this);
   }
+
+  renderSchedule() {
+    this.setState({ schedules: [1, 2, 3, 4] });
+  }
+
   render() {
+    const schedules = [];
+    for (let i = 0; i < this.state.schedules.length; i++) {
+      schedules.push(<Schedule />);
+    }
     return (
-      <div className="flex-container">
-        <div className="draft">
-          <List>
-            <ListItem divider="true">
-              <ListItemText primary="9:00" />
-            </ListItem>
-            <ListItem divider="true">
-              <ListItemText primary="10:00" />
-            </ListItem>
-            <ListItem divider="true">
-              <ListItemText primary="11:00" />
-            </ListItem>
-            <ListItem divider="true">
-              <ListItemText primary="12:00" />
-              <List>
-                <ListItem>
-                  <Checkbox />
-                  <ListItemText primary="done" />
-                </ListItem>
-                <ListItem>
-                  <Checkbox />
-                  <ListItemText primary="done" />
-                </ListItem>
-                <ListItem>
-                  <Checkbox />
-                  <ListItemText primary="done" />
-                </ListItem>
-              </List>
-            </ListItem>
-            <ListItem divider="true">
-              <ListItemText primary="1:00" />
-            </ListItem>
-            <ListItem divider="true">
-              <ListItemText primary="2:00" />
-            </ListItem>
-            <ListItem divider="true">
-              <ListItemText primary="3:00" />
-            </ListItem>
-            <ListItem divider="true">
-              <ListItemText primary="4:00" />
-            </ListItem>
-            <ListItem divider="true">
-              <ListItemText primary="5:00" />
-            </ListItem>
-            <ListItem divider="true">
-              <ListItemText primary="6:00" />
-            </ListItem>
-            <ListItem divider="true">
-              <ListItemText primary="7:00" />
-            </ListItem>
-            <ListItem divider="true">
-              <ListItemText primary="8:00" />
-            </ListItem>
-          </List>
+      <div>
+        <div className="buttons">
+          <Button variant="outlined" onClick={this.renderSchedule}>
+            Revise Schedule
+          </Button>
         </div>
-        <div className="revised">
-          <List>
-            <ListItem divider="true">
-              <ListItemText primary="9:00" />
-            </ListItem>
-            <ListItem divider="true">
-              <ListItemText primary="10:00" />
-            </ListItem>
-            <ListItem divider="true">
-              <ListItemText primary="11:00" />
-            </ListItem>
-            <ListItem divider="true">
-              <ListItemText primary="12:00" />
-              <List>
-                <ListItem>
-                  <Checkbox />
-                  <ListItemText primary="done" />
-                </ListItem>
-                <ListItem>
-                  <Checkbox />
-                  <ListItemText primary="done" />
-                </ListItem>
-                <ListItem>
-                  <Checkbox />
-                  <ListItemText primary="done" />
-                </ListItem>
-              </List>
-            </ListItem>
-            <ListItem divider="true">
-              <ListItemText primary="1:00" />
-            </ListItem>
-            <ListItem divider="true">
-              <ListItemText primary="2:00" />
-            </ListItem>
-            <ListItem divider="true">
-              <ListItemText primary="3:00" />
-            </ListItem>
-            <ListItem divider="true">
-              <ListItemText primary="4:00" />
-            </ListItem>
-            <ListItem divider="true">
-              <ListItemText primary="5:00" />
-            </ListItem>
-            <ListItem divider="true">
-              <ListItemText primary="6:00" />
-            </ListItem>
-            <ListItem divider="true">
-              <ListItemText primary="7:00" />
-            </ListItem>
-            <ListItem divider="true">
-              <ListItemText primary="8:00" />
-            </ListItem>
-          </List>
-        </div>
+        <div className="wrapper">{schedules}</div>
       </div>
     );
   }
 }
 
 export default App;
+
+/* EXAMPLE 
+const schedules = [];
+for (let schedule in this.props.currentValue) {
+  schedules.push(<Container />);
+}
+return <div className="schedules">{schedules}</div>
+*/
---
+++ client/components/Schedule.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+
+class Schedule extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="schedule">
+        <div className="draft">
+          <List>
+            <ListItem divider="true">
+              <ListItemText primary="9:00" />
+            </ListItem>
+            <ListItem divider="true">
+              <ListItemText primary="10:00" />
+            </ListItem>
+            <ListItem divider="true">
+              <ListItemText primary="11:00" />
+            </ListItem>
+            <ListItem divider="true">
+              <ListItemText primary="12:00" />
+              <List>
+                <ListItem>
+                  <Checkbox />
+                  <ListItemText primary="done" />
+                </ListItem>
+              </List>
+            </ListItem>
+            <ListItem divider="true">
+              <ListItemText primary="1:00" />
+            </ListItem>
+            <ListItem divider="true">
+              <ListItemText primary="2:00" />
+            </ListItem>
+            <ListItem divider="true">
+              <ListItemText primary="3:00" />
+            </ListItem>
+            <ListItem divider="true">
+              <ListItemText primary="4:00" />
+            </ListItem>
+            <ListItem divider="true">
+              <ListItemText primary="5:00" />
+            </ListItem>
+            <ListItem divider="true">
+              <ListItemText primary="6:00" />
+            </ListItem>
+            <ListItem divider="true">
+              <ListItemText primary="7:00" />
+            </ListItem>
+            <ListItem divider="true">
+              <ListItemText primary="8:00" />
+            </ListItem>
+          </List>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Schedule;
